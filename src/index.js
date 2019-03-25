f4826c17cfe181820badf736256bd6e0fe076464
realdennis <realdennis777@gmail.com>
2019-03-25 16:00:04 +0800

    Check eventTarget intance

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import { useEffect } from 'react';
 const useEventTarget = Target => (...args) => {
   const off = () => Target.removeEventListener(...args);
   useEffect(() => {
+    if (!Target instanceof EventTarget) return;
     Target.addEventListener(...args);
     return off;
   }, []);
