ee8b906b43307540ab2a7fd059dd25a1cfb3640c
Dennis <realdennis777@gmail.com>
2019-09-14 00:03:44 +0800

    JS->TS (#4)
    * Update nested dependecy
    
    * JS->TS
    
    * integrate ts

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,29 +1,40 @@
-import createEventTargetHook from '../index.js';
+import createEventTargetHook from '../index';
 import { renderHook } from '@testing-library/react-hooks';
 
+interface callback {
+  (e: Event): void;
+}
+interface EventInfo {
+  eventName: string;
+  callback: callback;
+}
 class EventTarget {
+  eventList: EventInfo[];
   constructor() {
     this.eventList = [];
   }
   resetEventList() {
     this.eventList = [];
   }
-  addEventListener(event, callback) {
+  addEventListener(eventName: string, callback: callback) {
     this.eventList.push({
-      event: event,
+      eventName: eventName,
       callback: callback
     });
   }
-  removeEventListener(event, callback) {
+  removeEventListener(eventName: string, callback: callback) {
     for (let i = 0; i < this.eventList.length; i++) {
       if (
-        this.eventList[i].event == event &&
+        this.eventList[i].eventName == eventName &&
         this.eventList[i].callback == callback
       ) {
         this.eventList.splice(i, 1);
         break;
       }
     }
+  }
+  dispatchEvent(e: Event) {
+    return true;
   }
 }
 const window = new EventTarget();
@@ -46,7 +57,7 @@ it('should work fine when we call off', () => {
   const { result } = setUp();
   const [_, off] = result.current;
   expect(window.eventList.length).toBe(1);
-  off();
+  (off as () => void)();
   expect(window.eventList.length).toBe(0);
 });
 
@@ -55,4 +66,12 @@ it('should work fine when unmount', () => {
   expect(window.eventList.length).toBe(1);
   unmount();
   expect(window.eventList.length).toBe(0);
+});
+
+it('should throw error', () => {
+  try {
+    createEventTargetHook({} as EventTarget);
+  } catch (e) {
+    expect(e.message).toBe('Not an event target');
+  }
 });
